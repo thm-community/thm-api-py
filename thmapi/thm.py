5c8985cc9d7f0302b2da379f36dd96523e21f6b0
Szymon Borecki <szymex73@gmail.com>
2020-06-02 12:01:23 +0200

    Add room routes, bump to 0.4.0

--- thmapi/thm.py
+++ thmapi/thm.py
@@ -192,3 +192,66 @@ class THM(object):
         """
 
         return http_get(self.session, f'{root_url}/games/koth/recent/games')
+
+    #
+    # Room
+    #
+    def room_details(self, room_code) -> dict:
+        """
+        Gets details of a specific room
+
+        :param room_code: Room code
+        :return: Room data
+        """
+
+        return http_get(self.session, f'{root_url}/api/room/{room_code}', has_success=True)
+
+    def room_new_rooms(self) -> list:
+        """
+        Gets a list of 6 newest rooms
+
+        :return: List of newest rooms
+        """
+
+        return http_get(self.session, f'{root_url}/api/newrooms')
+
+    def room_graph_data(self, room_code, user_count=10) -> list:
+        """
+        Gets a list of top {user_count} users from a room for the graph
+
+        :param room_code: Room code
+        :type user_count: int
+        :param user_count: Amount of user the call should return
+        :return: List of users on the scoreboard
+        """
+
+        return http_get(self.session, f'{root_url}/api/getgraphdata/{user_count}/{room_code}')
+
+    def room_issues(self, room_code) -> dict:
+        """
+        Gets issues from a specific room
+
+        :param room_code: Room code
+        :return: Room issues
+        """
+
+        return http_get(self.session, f'{root_url}/api/get-issues/{room_code}', has_success=True)['data']
+
+    def room_votes(self, room_code) -> int:
+        """
+        Gets votes for a specific room
+
+        :param room_code: Room code
+        :return: Room votes
+        """
+
+        return http_get(self.session, f'{root_url}/api/get-votes/{room_code}') #/api/weekly-challenge-rooms
+
+    def room_weekly_challenges(self) -> list:
+        """
+        Gets a list of weekly challenge rooms
+
+        :return: List of weekly challenge rooms
+        """
+
+        return http_get(self.session, f'{root_url}/api/weekly-challenge-rooms')
