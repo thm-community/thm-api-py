8a91a16d67150cda758c6b2dadafbbbbe2c7682d
Szymon Borecki <szymex73@gmail.com>
2020-06-02 16:32:44 +0200

    Add a HTTP POST wrapper, bump to 0.5.1

--- thmapi/util.py
+++ thmapi/util.py
@@ -41,3 +41,45 @@ def http_get(session, path, res_format="json", has_success=False):
             raise Exception('HTTP Response was not 200')
     except Exception as err:
         raise err
+
+
+def http_post(session, path, data, res_format="json", has_success=False):
+    """
+    HTTP POST call wrapper
+
+    :type session: requests.Session
+    :param session: HTTP Session
+    :type path: string
+    :param path: HTTP Route
+    :type data: dict
+    :param data: HTTP Data (JSON)
+    :type res_format: string
+    :param res_format: Response format
+    :type has_success: bool
+    :param has_success: Does the request have a "success" field
+    :return: Return object
+    """
+
+    try:
+        r = session.post(f'{root_url}{path}', json=data)
+
+        if r.status_code == 200:
+            if res_format == 'json':
+                if has_success:
+                    res = r.json()
+
+                    if res['success']:
+                        return res
+                    else:
+                        if 'message' in res:
+                            raise Exception(f'API Error: {res["message"]}')
+                        else:
+                            raise Exception(f'API Error (no reason given)\nResponse: {res}')
+                else:
+                    return r.json()
+            else:
+                return r.content.decode('utf-8')
+        else:
+            raise Exception('HTTP Response was not 200')
+    except Exception as err:
+        raise err
